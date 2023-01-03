98b2043e18973b9536a8a733e71ee40dff1ea12f
MalchenkoV <malchenko.v.ek@gmail.com>
2023-01-03 13:22:53 +0400

    example1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Я сделяль!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Info</h1>
+        <nav className='menu'>
+          <ul>
+            <li><a href='#weather' className='link'>Weather</a></li>
+            <li><a href='#holiday' className='link'>Holiday</a></li>
+            <li><a href='#news' className='link'>News</a></li>
+          </ul>
+        </nav>
+        <div className='add-info'>
+          <p>Здесь будут интересные вещи</p>
+        </div>
       </header>
+      <main className='App-main'>
+        <section className='weather' id='weather'>
+            <div className='data-box'>
+              <h2 className='header'>Tbilisi</h2>
+              <h2 className='time'></h2>
+              <h2 className='temp'></h2>
+              <h2 className='wind'></h2>              
+            </div>
+        </section>
+        <section className='holiday' id='holiday'>
+          <div className='data-box'>
+            <h2 className='header'>Today is</h2>
+            <h2 className='day-today'></h2>
+          </div>
+        </section>
+        <section className='news' id='news'>
+          <div className='data-box'>
+            <h2 className='title'></h2>
+            <a href='' className='url' target="_blank" rel="noopener noreferrer">Show me articles</a>
+          </div>
+        </section>
+      </main>
+      <footer className='App-footer'>@Created by MalchenkoV</footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import store from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
